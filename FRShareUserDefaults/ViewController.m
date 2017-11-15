b8bcf90c24f790672d1876c9205c46a107bc0fb6
frank <fanxiushan@gmail.com>
2017-11-15 16:39:59 +0800

    Initial Commit

---
+++ FRShareUserDefaults/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FRShareUserDefaults
+//
+//  Created by Fan's iMac  on 2017/11/15.
+//  Copyright © 2017年 Fan's iMac . All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
